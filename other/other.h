d9753b1ff47933e909015511a4833acac614a403
Jean-Philippe Ouellet <jpo@vt.edu>
2014-04-13 16:09:24 -0400

    Add support for OS X. Tested on 10.6.8 and 10.9.1.

--- other/other.h
+++ other/other.h
@@ -3,7 +3,9 @@
 
 #define _PATH_TTY "/dev/tty"
 
+#ifndef HAVE_STRLCAT
 size_t strlcat(char *dst, const char *src, size_t siz);
+#endif
 void explicit_bzero(void *b, size_t len);
 int bcrypt_pbkdf(const char *pass, size_t pass_len, const uint8_t *salt,
          size_t salt_len, uint8_t *key, size_t key_len, unsigned int rounds);
