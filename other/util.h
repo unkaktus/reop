8b6f63a5bfcb7c01e9c1cbb30b06fe0844ae3235
Ted Unangst <tedu@tedunangst.com>
2015-04-05 07:20:11 -0400

    try harder to configure strlcpy on darwin

--- other/util.h
+++ other/util.h
@@ -1,12 +1,9 @@
 #include <sys/types.h>
 #include <inttypes.h>
 
-#ifdef NEED_STRLCAT
-size_t strlcat(char *dst, const char *src, size_t siz);
-#endif
-
 #ifdef NEED_STRLCPY
 size_t strlcpy(char *dst, const char *src, size_t siz);
+size_t strlcat(char *dst, const char *src, size_t siz);
 #endif
 
 int bcrypt_pbkdf(const char *pass, size_t pass_len, const uint8_t *salt,
